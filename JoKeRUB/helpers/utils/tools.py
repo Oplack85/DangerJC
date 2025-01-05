8da6d5d9ca5fe1cb74d3815ecd7b587f2958dd68
Oplack85 <hayd34500@gmail.com>
2025-01-05 18:25:11 +0300

    Add files via upload

--- JoKeRUB/helpers/utils/tools.py
+++ JoKeRUB/helpers/utils/tools.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
 
-from moviepy.editor import VideoFileClip
+from moviepy import *
 from PIL import Image
 
 from ...core.logger import logging
@@ -26,59 +26,59 @@ async def media_to_pic(event, reply, noedits=False):  # sourcery no-metrics
     ]:
         return event, None
     if not noedits:
-        catevent = await edit_or_reply(
-            event, "`جار التحويل قد يستغرق الوقت الى قليل`"
+        zedevent = await edit_or_reply(
+            event, "`Transfiguration Time! Converting to ....`"
         )
 
     else:
-        catevent = event
-    catmedia = None
-    catfile = os.path.join("./temp/", "meme.png")
-    if os.path.exists(catfile):
-        os.remove(catfile)
+        zedevent = event
+    zedmedia = None
+    zedfile = os.path.join("./temp/", "meme.png")
+    if os.path.exists(zedfile):
+        os.remove(zedfile)
     if mediatype == "Photo":
-        catmedia = await reply.download_media(file="./temp")
-        im = Image.open(catmedia)
-        im.save(catfile)
+        zedmedia = await reply.download_media(file="./temp")
+        im = Image.open(zedmedia)
+        im.save(zedfile)
     elif mediatype in ["Audio", "Voice"]:
-        await event.client.download_media(reply, catfile, thumb=-1)
+        await event.client.download_media(reply, zedfile, thumb=-1)
     elif mediatype == "Sticker":
-        catmedia = await reply.download_media(file="./temp")
-        if catmedia.endswith(".tgs"):
-            catcmd = f"lottie_convert.py --frame 0 -if lottie -of png '{catmedia}' '{catfile}'"
-            stdout, stderr = (await runcmd(catcmd))[:2]
+        zedmedia = await reply.download_media(file="./temp")
+        if zedmedia.endswith(".tgs"):
+            zedcmd = f"lottie_convert.py --frame 0 -if lottie -of png '{zedmedia}' '{zedfile}'"
+            stdout, stderr = (await runcmd(zedcmd))[:2]
             if stderr:
                 LOGS.info(stdout + stderr)
-        elif catmedia.endswith(".webm"):
-            clip = VideoFileClip(catmedia)
+        elif zedmedia.endswith(".webm"):
+            clip = VideoFileClip(zedmedia)
             try:
-                clip = clip.save_frame(catfile, 0.1)
+                clip = clip.save_frame(zedfile, 0.1)
             except Exception:
-                clip = clip.save_frame(catfile, 0)
-        elif catmedia.endswith(".webp"):
-            im = Image.open(catmedia)
-            im.save(catfile)
+                clip = clip.save_frame(zedfile, 0)
+        elif zedmedia.endswith(".webp"):
+            im = Image.open(zedmedia)
+            im.save(zedfile)
     elif mediatype in ["Round Video", "Video", "Gif"]:
-        await event.client.download_media(reply, catfile, thumb=-1)
-        if not os.path.exists(catfile):
-            catmedia = await reply.download_media(file="./temp")
-            clip = VideoFileClip(catmedia)
+        await event.client.download_media(reply, zedfile, thumb=-1)
+        if not os.path.exists(zedfile):
+            zedmedia = await reply.download_media(file="./temp")
+            clip = VideoFileClip(zedmedia)
             try:
-                clip = clip.save_frame(catfile, 0.1)
+                clip = clip.save_frame(zedfile, 0.1)
             except Exception:
-                clip = clip.save_frame(catfile, 0)
+                clip = clip.save_frame(zedfile, 0)
     elif mediatype == "Document":
         mimetype = reply.document.mime_type
         mtype = mimetype.split("/")
         if mtype[0].lower() == "image":
-            catmedia = await reply.download_media(file="./temp")
-            im = Image.open(catmedia)
-            im.save(catfile)
-    if catmedia and os.path.lexists(catmedia):
-        os.remove(catmedia)
-    if os.path.lexists(catfile):
-        return catevent, catfile, mediatype
-    return catevent, None
+            zedmedia = await reply.download_media(file="./temp")
+            im = Image.open(zedmedia)
+            im.save(zedfile)
+    if zedmedia and os.path.lexists(zedmedia):
+        os.remove(zedmedia)
+    if os.path.lexists(zedfile):
+        return zedevent, zedfile, mediatype
+    return zedevent, None
 
 
 async def take_screen_shot(
